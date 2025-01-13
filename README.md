1a8adf0aeccd5e33666a57c203ac2a60d7dcda88
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-01-13 00:50:57 +0000

    Adjust bot for RoboHack event

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ A discord bot to gate the entry of a discord server on multiple passwords (each 
 
 The use case is as follows:
 
-- Send different people/groups the same discord link, with a different password.
-- Each user is prompted for their password on entry. If they enter the correct password, they are given their appropriate role.
+-   Send different people/groups the same discord link, with a different password.
+-   Each user is prompted for their password on entry. If they enter the correct password, they are given their appropriate role.
 
 ## Requirements
 
@@ -17,17 +17,16 @@ For development, see `script/requirements.txt`
 
 # Discord server set-up instructions
 
-- Ensure the `@everyone` role cannot see any channels by default.
-- Create a role named `Verified` which can see the base channels (i.e. #general)
-- Create a role named `Unverified Volunteer` which can see the volunteer onboarding channel.
-- Create a role named `Blueshirt`.
-- Create a role named `Team Supervisor`.
-- Create a new channel category called `welcome`, block all users from reading this category in its permissions.
-- Create channel categories called `Team Channels` and `Team Voice Channels`.
-- Create a channel named `#blog`, block all users from sending messages in it.
-- Set a Blueshirt password using `/passwd tla:SRZ new_password:`
-- Make sure that the SRbot role is at the top of the role list (it can only assign roles below its own)
-- Create teams using the `/team new` command.
+-   Ensure the `@everyone` role cannot see any channels by default.
+-   Create a role named `Participant` which can see the base channels (i.e. #general)
+-   Create a role named `Unverified Crew` which can see the crew onboarding channel.
+-   Create a role named `Crew`.
+-   Create a new channel category called `welcome`, block all users from reading this category in its permissions.
+-   Create channel categories called `Team Channels` and `Team Voice Channels`.
+-   Create a channel named `#announcements`, block all users from sending messages in it.
+-   Set a crew password using `/passwd tla:SRO new_password:`
+-   Make sure that the SRbot role is at the top of the role list (it can only assign roles below its own)
+-   Create teams using the `/team new` command.
 
 And voilà, any new users should automatically get their role assigned once they enter the correct password.
 
--- src/sr/discord_bot/commands/team.py
+++ src/sr/discord_bot/commands/team.py
@@ -10,7 +10,6 @@ if TYPE_CHECKING:
 
 from sr.discord_bot.constants import (
     ROLE_PREFIX,
-    TEAM_LEADER_ROLE,
     TEAM_CATEGORY_NAME,
     TEAM_CHANNEL_PREFIX,
     TEAM_VOICE_CATEGORY_NAME,
@@ -29,8 +28,11 @@ class Team(app_commands.Group):
 group = Team()
 
 
-def permissions(client: "BotClient", team: discord.Role) -> Mapping[
-    discord.Role | discord.Member, discord.PermissionOverwrite,
+def permissions(
+    client: "BotClient", team: discord.Role
+) -> Mapping[
+    discord.Role | discord.Member,
+    discord.PermissionOverwrite,
 ]:
     if not isinstance(client.guild, discord.Guild):
         return {}
@@ -52,16 +54,20 @@ def permissions(client: "BotClient", team: discord.Role) -> Mapping[
 
 
 @group.command(  # type:ignore[arg-type]
-    name='new',
-    description='Creates a role and channel for a team',
+    name="new",
+    description="Creates a role and channel for a team",
 )
 @app_commands.describe(
-    tla='Three Letter Acronym (e.g. SRZ)',
-    name='Name of the team',
+    tla="Three Letter Acronym (e.g. SRO)",
+    name="Name of the team",
     password="Password required for joining the team",
 )
-async def new_team(interaction: discord.interactions.Interaction["BotClient"], tla: str, name: str,
-                   password: str) -> None:
+async def new_team(
+    interaction: discord.interactions.Interaction["BotClient"],
+    tla: str,
+    name: str,
+    password: str,
+) -> None:
     guild: discord.Guild | None = interaction.guild
     if guild is None:
         raise app_commands.NoPrivateMessage()
@@ -70,7 +76,9 @@ async def new_team(interaction: discord.interactions.Interaction["BotClient"], t
     role_name = f"{ROLE_PREFIX}{tla.upper()}"
 
     if discord.utils.get(guild.roles, name=role_name) is not None:
-        await interaction.response.send_message(f"{role_name} already exists", ephemeral=True)
+        await interaction.response.send_message(
+            f"{role_name} already exists", ephemeral=True
+        )
         return
 
     role = await guild.create_role(
@@ -86,33 +94,47 @@ async def new_team(interaction: discord.interactions.Interaction["BotClient"], t
         overwrites=permissions(interaction.client, role),
     )
     interaction.client.set_password(tla, password)
-    await interaction.response.send_message(f"{role.mention} and {channel.mention} created!", ephemeral=True)
+    await interaction.response.send_message(
+        f"{role.mention} and {channel.mention} created!", ephemeral=True
+    )
 
 
 @group.command(  # type:ignore[arg-type]
-    name='delete',
-    description='Deletes a role and channel for a team',
+    name="delete",
+    description="Deletes a role and channel for a team",
 )
 @app_commands.describe(
-    tla='Three Letter Acronym (e.g. SRZ)',
+    tla="Three Letter Acronym (e.g. SRO)",
 )
-async def delete_team(interaction: discord.interactions.Interaction["BotClient"], tla: str) -> None:
+async def delete_team(
+    interaction: discord.interactions.Interaction["BotClient"], tla: str
+) -> None:
     guild: discord.Guild | None = interaction.guild
     if guild is None:
         raise app_commands.NoPrivateMessage()
-    role: discord.Role | None = discord.utils.get(guild.roles, name=f"{ROLE_PREFIX}{tla.upper()}")
+    role: discord.Role | None = discord.utils.get(
+        guild.roles, name=f"{ROLE_PREFIX}{tla.upper()}"
+    )
 
     if role is None:
-        await interaction.response.send_message(f"Team {tla.upper()} does not exist", ephemeral=True)
+        await interaction.response.send_message(
+            f"Team {tla.upper()} does not exist", ephemeral=True
+        )
         return
 
     view = TeamDeleteConfirm(guild, tla)
 
-    await interaction.response.send_message(f"Are you sure you want to delete Team {tla.upper()}?\n\n"
-                                            f"This will kick all of its members.", view=view, ephemeral=True)
+    await interaction.response.send_message(
+        f"Are you sure you want to delete Team {tla.upper()}?\n\n"
+        f"This will kick all of its members.",
+        view=view,
+        ephemeral=True,
+    )
     await view.wait()
     if view.value:
-        await interaction.edit_original_response(content=f"_Deleting Team {tla.upper()}..._", view=None)
+        await interaction.edit_original_response(
+            content=f"_Deleting Team {tla.upper()}..._", view=None
+        )
         reason = f"Team removed by {interaction.user.name}"
         if role is not None:
             for member in role.members:
@@ -126,30 +148,39 @@ async def delete_team(interaction: discord.interactions.Interaction["BotClient"]
             await role.delete(reason=reason)
             interaction.client.remove_password(tla)
 
-            if (
-                isinstance(interaction.channel, discord.abc.GuildChannel)
-                and not interaction.channel.name.startswith(f"{TEAM_CHANNEL_PREFIX}{tla.lower()}")
+            if isinstance(
+                interaction.channel, discord.abc.GuildChannel
+            ) and not interaction.channel.name.startswith(
+                f"{TEAM_CHANNEL_PREFIX}{tla.lower()}"
             ):
-                await interaction.edit_original_response(content=f"Team {tla.upper()} has been deleted")
+                await interaction.edit_original_response(
+                    content=f"Team {tla.upper()} has been deleted"
+                )
     else:
         await interaction.delete_original_response()
 
 
 @group.command(  # type:ignore[arg-type]
-    name='voice',
-    description='Create a voice channel for a team',
+    name="voice",
+    description="Create a voice channel for a team",
 )
 @app_commands.describe(
-    tla='Three Letter Acronym (e.g. SRZ)',
+    tla="Three Letter Acronym (e.g. SRO)",
 )
-async def create_voice(interaction: discord.interactions.Interaction["BotClient"], tla: str) -> None:
+async def create_voice(
+    interaction: discord.interactions.Interaction["BotClient"], tla: str
+) -> None:
     guild: discord.Guild | None = interaction.guild
     if guild is None:
         raise app_commands.NoPrivateMessage()
 
-    role: discord.Role | None = discord.utils.get(guild.roles, name=f"{ROLE_PREFIX}{tla.upper()}")
+    role: discord.Role | None = discord.utils.get(
+        guild.roles, name=f"{ROLE_PREFIX}{tla.upper()}"
+    )
     if role is None:
-        await interaction.response.send_message(f"Team {tla.upper()} does not exist", ephemeral=True)
+        await interaction.response.send_message(
+            f"Team {tla.upper()} does not exist", ephemeral=True
+        )
         return
 
     category = discord.utils.get(guild.categories, name=TEAM_VOICE_CATEGORY_NAME)
@@ -158,16 +189,18 @@ async def create_voice(interaction: discord.interactions.Interaction["BotClient"
         category=category,
         overwrites=permissions(interaction.client, role),
     )
-    await interaction.response.send_message(f"{channel.mention} created!", ephemeral=True)
+    await interaction.response.send_message(
+        f"{channel.mention} created!", ephemeral=True
+    )
 
 
 @group.command(  # type:ignore[arg-type]
-    name='channel',
-    description='Create a secondary channel for a team',
+    name="channel",
+    description="Create a secondary channel for a team",
 )
 @app_commands.describe(
-    tla='Three Letter Acronym (e.g. SRZ)',
-    suffix='Channel name suffix (e.g. design)',
+    tla="Three Letter Acronym (e.g. SRO)",
+    suffix="Channel name suffix (e.g. design)",
 )
 async def create_team_channel(
     interaction: discord.interactions.Interaction["BotClient"],
@@ -178,16 +211,22 @@ async def create_team_channel(
     if guild is None:
         raise app_commands.NoPrivateMessage()
 
-    role: discord.Role | None = discord.utils.get(guild.roles, name=f"{ROLE_PREFIX}{tla.upper()}")
+    role: discord.Role | None = discord.utils.get(
+        guild.roles, name=f"{ROLE_PREFIX}{tla.upper()}"
+    )
     if role is None:
         await interaction.response.send_message("Team does not exist", ephemeral=True)
         return
 
-    main_channel = discord.utils.get(guild.text_channels, name=f"{TEAM_CHANNEL_PREFIX}{tla.lower()}")
+    main_channel = discord.utils.get(
+        guild.text_channels, name=f"{TEAM_CHANNEL_PREFIX}{tla.lower()}"
+    )
     category = discord.utils.get(guild.categories, name=TEAM_CATEGORY_NAME)
 
     if category is None or main_channel is None:
-        await interaction.response.send_message(f"Team {tla.upper()} does not exist", ephemeral=True)
+        await interaction.response.send_message(
+            f"Team {tla.upper()} does not exist", ephemeral=True
+        )
         return
 
     new_channel = await guild.create_text_channel(
@@ -197,7 +236,9 @@ async def create_team_channel(
         position=main_channel.position + 1,
         reason=TEAM_CREATED_REASON,
     )
-    await interaction.response.send_message(f"{new_channel.mention} created!", ephemeral=True)
+    await interaction.response.send_message(
+        f"{new_channel.mention} created!", ephemeral=True
+    )
 
 
 async def _export_team(
@@ -206,20 +247,30 @@ async def _export_team(
     guild: discord.Guild,
     interaction: discord.interactions.Interaction["BotClient"],
 ) -> str:
-    main_channel = discord.utils.get(guild.text_channels, name=f"team-{team_tla.lower()}")
+    main_channel = discord.utils.get(
+        guild.text_channels, name=f"team-{team_tla.lower()}"
+    )
     if main_channel is None and not isinstance(main_channel, discord.abc.GuildChannel):
         raise app_commands.AppCommandError("Invalid TLA")
 
     password = interaction.client.passwords[team_tla]
-    commands = [f"/team new tla:{team_tla} name:{main_channel.topic} password:{password}"]
+    commands = [
+        f"/team new tla:{team_tla} name:{main_channel.topic} password:{password}"
+    ]
 
     if not only_teams:
-        channels = filter(lambda c: c.name.startswith(f"team-{team_tla.lower()}-"), guild.text_channels)
+        channels = filter(
+            lambda c: c.name.startswith(f"team-{team_tla.lower()}-"),
+            guild.text_channels,
+        )
         for channel in channels:
             suffix = channel.name.removeprefix(f"team-{team_tla.lower()}-")
             commands.append(f"/team channel tla:{team_tla} suffix:{suffix}")
 
-        has_voice: bool = discord.utils.get(guild.voice_channels, name=f"team-{team_tla.lower()}") is not None
+        has_voice: bool = (
+            discord.utils.get(guild.voice_channels, name=f"team-{team_tla.lower()}")
+            is not None
+        )
         if has_voice:
             commands.append(f"/team voice tla:{team_tla}")
 
@@ -228,11 +279,11 @@ async def _export_team(
 
 
 @group.command(  # type:ignore[arg-type]
-    name='export',
-    description='Outputs all commands needed to create a team (or all teams)',
+    name="export",
+    description="Outputs all commands needed to create a team (or all teams)",
 )
 @app_commands.describe(
-    tla='Three Letter Acronym (e.g. SRZ)',
+    tla="Three Letter Acronym (e.g. SRO)",
     only_teams="Only creates teams without extra channels",
 )
 async def export_team(
@@ -250,8 +301,13 @@ async def export_team(
 
     if tla is None:
         for team_role in guild.roles:
-            if team_role.name.startswith(ROLE_PREFIX) and team_role.name != TEAM_LEADER_ROLE:
-                output += await _export_team(team_role.name.removeprefix(ROLE_PREFIX), only_teams, guild, interaction)
+            if team_role.name.startswith(ROLE_PREFIX):
+                output += await _export_team(
+                    team_role.name.removeprefix(ROLE_PREFIX),
+                    only_teams,
+                    guild,
+                    interaction,
+                )
     else:
         output = output + await _export_team(tla, only_teams, guild, interaction)
     output = output + "\n```"
@@ -263,10 +319,12 @@ def _repair_permissions_status_msg(cur_team: int, num_teams: int) -> str:
 
 
 @group.command(  # type:ignore[arg-type]
-    name='repair-permissions',
-    description='Reset channel and team permissions',
+    name="repair-permissions",
+    description="Reset channel and team permissions",
 )
-async def repair_permissions(interaction: discord.interactions.Interaction["BotClient"]) -> None:
+async def repair_permissions(
+    interaction: discord.interactions.Interaction["BotClient"],
+) -> None:
     guild: discord.Guild | None = interaction.guild
     if guild is None:
         raise app_commands.NoPrivateMessage()
@@ -277,7 +335,9 @@ async def repair_permissions(interaction: discord.interactions.Interaction["BotC
         thinking=True,
         ephemeral=True,
     )
-    await interaction.edit_original_response(content=_repair_permissions_status_msg(0, len(team_roles)))
+    await interaction.edit_original_response(
+        content=_repair_permissions_status_msg(0, len(team_roles))
+    )
 
     for index, role in enumerate(team_roles):
         tla = role.name.removeprefix(ROLE_PREFIX)
@@ -291,6 +351,8 @@ async def repair_permissions(interaction: discord.interactions.Interaction["BotC
             for channel_role, overwrites in channel_permissions.items():
                 await channel.set_permissions(target=channel_role, overwrite=overwrites)
         await role.edit(mentionable=True)
-        await interaction.edit_original_response(content=_repair_permissions_status_msg(index + 1, len(team_roles)))
+        await interaction.edit_original_response(
+            content=_repair_permissions_status_msg(index + 1, len(team_roles))
+        )
 
     await interaction.edit_original_response(content="Repairing permissions... done!")
